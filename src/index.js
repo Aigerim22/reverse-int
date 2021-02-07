0ab12c888e02f6c85c26b9d810357f59f2714cc9
Aigerim Tazhibay <aika0822t@gmail.com>
2021-02-08 00:52:59 +0600

    init: start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const { parse } = require("semver");
+
 module.exports = function reverse (n) {
-  
+  var str = n.toString();
+  var arr = str.split("");
+  var reversed = arr.reverse();
+  var s = arr.join("");
+  n = parseInt(s);
+  return n;
 }
